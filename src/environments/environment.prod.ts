d412048614fc75b03fd2edb343968d8b8184a6e0
Benjamin Schurtenberger <benjamin.schurtenberger@gmail.com>
2022-05-25 09:06:40 +0200

    push environment.ts & environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+   production: true,
+   GOOGLE_GEOCODING_API_URL: "",
+   GOOGLE_GEOCODING_API_KEY: ""
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+   production: false,
+   GOOGLE_GEOCODING_API_URL: "",
+   GOOGLE_GEOCODING_API_KEY: ""
+};
