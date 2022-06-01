2ac1a61fca7ba7c2d7b37c83cce1c4a47ed18e32
Benjamin Schurtenberger <benjamin.schurtenberger@gmail.com>
2022-06-01 08:31:03 +0200

    cleanup unused items, logos, headers, etc, adding width styling to input-searchbar

--- src/app/input-searchbar/input-searchbar.module.ts
+++ src/app/input-searchbar/input-searchbar.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchbarComponent } from './input-searchbar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MaterialModule } from 'src/material.module';
 
 @NgModule({
