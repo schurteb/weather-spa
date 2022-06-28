46ddea1f871fc93804c3d47ec2f18cad67211b73
schurteb <benjamin.schurtenberger@gmail.com>
2022-06-28 22:59:23 +0200

    Remove secrets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
    production: false,
    GOOGLE_GEOCODING_API_URL: "https://maps.googleapis.com/maps/api/geocode/json",
-   GOOGLE_GEOCODING_API_KEY: "AIzaSyAoQuqhy2zqVtOMAkW_dA7g5VUa2M_v5Qk",
+   GOOGLE_GEOCODING_API_KEY: "",
    OPENWEATHERMAP_API_URL: "https://api.openweathermap.org/data/3.0/onecall",
-   OPENWEATHERMAP_API_KEY: "4de6f4ad027b40a3e299e036c89789c3"
+   OPENWEATHERMAP_API_KEY: ""
 };
