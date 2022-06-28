0f1c5a3b6c2d9699aa0b29ad0d478d2b58234fab
Benjamin Schurtenberger <benjamin.schurtenberger@gmail.com>
2022-06-28 19:29:04 +0200

    cleanup

--- src/app/weatherData/weatherData.component.ts
+++ src/app/weatherData/weatherData.component.ts
@@ -1,6 +1,5 @@
-import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
-import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Coordinates } from '../classes/coordinates';
 import { buildOpenWeatherMapRequest, Forecast, Units } from 'owm-onecall-api';
@@ -39,31 +38,6 @@ export class WeatherDataComponent {
         this.weatherData = value;
       })
       .catch(console.log);
-
-    let options/*: {
-      headers?: HttpHeaders | {[header: string]: string | string[]},
-      observe?: 'body' | 'events' | 'response',
-      params?: HttpParams|{[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>},
-      reportProgress?: boolean,
-      responseType?: 'arraybuffer'|'blob'|'json'|'text',
-      withCredentials?: boolean,
-    }*/ = {
-      //headers: new HttpHeaders("API:" + environment.GOOGLE_GEOCODING_API_KEY),
-    }
-
-
-    // Create request to Google's Geocoding API
-    /*this.http.get(
-      environment.OPENWEATHERMAP_API_URL + "?lat=" + c.latitude + "&lon=" + c.longitude + "&units=metric" + "&appid=" + environment.OPENWEATHERMAP_API_KEY,
-      options
-    ).subscribe((value: any) => {
-
-      console.log(value);
-      this.weatherDataStr = JSON.stringify(value);
-
-      // Cast response to it's correct type
-
-    })*/
   }
 
   parseDate(unix: number): Date {
